5ed2ffb4f5b1a75f05902e202550eba022925c7a
wjohnson8 <110488684+wjohnson8@users.noreply.github.com>
2025-03-13 11:01:47 -0400

    Update TestCases.py
    Added top level test case and fixed print function.

--- TestCases.py
+++ TestCases.py
@@ -9,7 +9,7 @@ time.sleep(0.25)
 
 
 #Code snippet 13: Function for Printing Data
-def print_data(slave_data_test,concentration):
+def print_data(readingData, ledTempData, photoTempData, modeLog, concentration):
     timestamp = time.localtime() # Generate a filename with a timestamp for exel file
     exeltime = str(timestamp) # Convert timestamp to a string format to be incorporated into the csv
 
@@ -32,18 +32,20 @@ def print_data(slave_data_test,concentration):
     file = open(filename, "w") # This file can later be automated to automatically be renamed to today's date with a timestamp.
 
     #file.write(exeltime) # Write the timestamp on the first row of the csv file.
-    file.write("\n") # Next row.
-    file.write("reading,voltage,concentration") # Label 3 columns on the second row of the csv file.
+    file.write("reading,voltage,LED_temp,photo_temp,mode,concentration") # Label 3 columns on the first row of the csv file.
     file.write("\n") # Next row.
     file.flush() # Flushes file. This effectively saves the file without closing it.
 
 
-    for i in range(len(slave_data_test)): # For each reading in the reading array...
-        value = str(slave_data_test[i]) # Convert the voltage reading to a string.
-        reading = str(i) # Convert the reading number to a string.
-        
+    for i in range(len(readingData)): # For each reading in the reading array...
+        value = str(readingData[i]) # Convert the voltage reading to a string.
+        reading = str(i/Functions.freq) # Convert the reading number to a string.
+        ledTemp=str(ledTempData[i])
+        photoTemp=str(photoTempData[i])
+        mode=modeLog[i]
+
         # Write the reading number and corresponding reading voltage row by row.
-        file.write(reading+","+value+","+concentration)
+        file.write(reading+","+value+","+ledTemp+","+photoTemp+","+mode+","+concentration)
         file.write("\n")
     file.flush() # Flushes file
 
@@ -53,9 +55,25 @@ def print_data(slave_data_test,concentration):
 
 
 
+#Top Level Testcase
 concentration="100"
+# Create an empty list to store voltage values
+readingData = []
+# Create an empty list to store temperature reading values for LEDs
+ledTempData = []
+# Create an empty list to store temperature reading values for photodiode
+photoTempData = []
+# Create an empty list to store data collection mode. Either 1550, 1200, or both.
+modeLog = []
+
+readingData, ledTempData, photoTempData, modeLog = Functions.topLevelDataCollection(2)
+print_data(readingData, ledTempData, photoTempData, modeLog, concentration)
+
+
+
 
 """
+#Individual Mode Test Cases
 #1550
 print("LED on")
 Functions.LED_control('1550', True)
@@ -133,22 +151,21 @@ print("LED off again")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+"""
+#Read temperature test cases
 #read temp
 print("Measuring temperature")
-t = Functions.readTemp()# Store temperature measurement here
+t = Functions.readTemp(Functions.CS_PHOTODIODE_TEMP)# Store temperature measurement here
 print(t)
-
+#read temp
+print("Measuring temperature")
+t = Functions.readTemp(Functions.CS_1550NM)# Store temperature measurement here
+print(t)
+#read temp
+print("Measuring temperature")
+t = Functions.readTemp(Functions.CS_1200NM)# Store temperature measurement here
+print(t)
+"""
 
 
 
@@ -164,7 +181,7 @@ print("data collection complete")
 """
 
 
-"""SS
+"""
 print("beginning LED on/off testcase")
 Functions.LED_control('1550', False)
 Functions.LED_control('1200', False)
@@ -180,46 +197,7 @@ print("ending LED on/off testcase")
 """
 
 
-"""
-#Code snippet 14: Motor Rotation Test Case
-#Motor rotation 180 degree test case
-try:
-    # Parameters for motor rotation
-    angle = 30           # Rotate 180 degrees
-    step_angle = 0.018      # Step angle of the motor (in degrees)
-    delay_per_step = 0.001 # Delay between steps (for 5 second rotation)
-    direction = 0        # 1 for counterclockwise, 0 for clockwise-->from top
-    m1, m2, m3 = 0, 0, 0  # Set stepping factor to full step because of minimum step angle
 
-
-    print(f"Rotating {angle/2} degrees clockwise with 1/16 microstepping...")
-    Functions.motorRotation(angle, step_angle, delay_per_step, direction, m1, m2, m3)
-    print("Rotation complete.")
-except KeyboardInterrupt:
-    print("Exiting program...")
-    Functions.enable_pin.value(1)  # Disable the motor driver
-
-
-
-
-print("beginning data collection")
-# Initialize an empty result list
-result = []
-# Call the function 24 times and concatenate the results
-for _ in range(24):  # Loop 24 times
-    # Parameters for motor rotation
-    angle = 0.25           # Rotate 180 degrees
-    step_angle = 0.018      # Step angle of the motor (in degrees)
-    delay_per_step = 0.001 # Delay between steps (for 5 second rotation)
-    direction = 0        # 1 for counterclockwise, 0 for clockwise-->from top
-    m1, m2, m3 = 0, 0, 0  # Set stepping factor to full step because of minimum step angle
-
-    Functions.motorRotation(angle, step_angle, delay_per_step, direction, m1, m2, m3)
-    current_array = Functions.collectData(channel=3)  # Get the 5-element array from the function
-    result.extend(current_array)  # Append the elements to the result list
-print_data(result)
-print("data collection complete")
-"""
 
 
 
